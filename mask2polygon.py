5920974214e900fe481b256003e187dd771f7c70
Sami Barchid <sami.barchid@univ-lille.fr>
2022-01-10 00:03:46 +0100

    mask2polygon for oxford

--- mask2polygon.py
+++ mask2polygon.py
@@ -6,36 +6,83 @@ import torch
 import numpy as np
 from project.datamodules.oxford_iiit_pet import OxfordPetDatamodule, preprocess_mask
 import matplotlib.pyplot as plt
+import argparse
+import json
 
-POLYGON_POINTS = 19  # polygon annotation of 11 vertices
+
+def is_clockwise(contour):
+    sum = 0.
+    pt1 = 0
+    pt2 = 1
+    for _ in range(len(contour) - 1):
+        x1, y1 = contour[pt1]
+        x2, y2 = contour[pt2]
+
+        sum += (x2 - x1) * (y2 + y1)
+
+        pt1 += 1
+        pt2 += 1
+
+    # link last point to first point
+    x1, y1 = contour[-1]
+    x2, y2 = contour[0]
+    sum += (x2 - x1) * (y2 + y1)
+
+    return sum >= 0
 
 
-def find_polygon(mask: np.ndarray):
+def find_polygon(mask: np.ndarray, polygon_vertices):
     # get contour from mask
     cnts = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    
+    if len(cnts) == 0:
+        print('no contours in the mask.')
+        return None
+    
     c = max(cnts, key=cv2.contourArea)
+
+    # return cv2.convexHull(c, clockwise=False)
     peri = cv2.arcLength(c, True)
 
     for i in range(1, 1000):
         eps = i * 0.0001
         approx = cv2.approxPolyDP(c, eps * peri, True)
 
-        if len(approx) == POLYGON_POINTS:
-            print('FOUND ! After ', i, 'iterations')
+        if len(approx) == polygon_vertices:
+            # print('FOUND ! After ', i, 'iterations')
             return approx
-        if len(approx) < POLYGON_POINTS:
-            print('Not found exact match... Approximation length :', len(approx))
+        if len(approx) < polygon_vertices:
+            # print('Not found exact match... Approximation length :', len(approx))
+            
+            # if the approximate is shorter than polygon_vertices, adds the same last point
+            missed = polygon_vertices - len(approx)
+            for _ in range(missed):
+                approx = np.append(approx, np.expand_dims(approx[-1], axis=0), axis=0)
+                
             return approx
+        
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default="data/oxford_iiit_pet")
+    parser.add_argument('--polygon_vertices', type=int, default=19)
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
+    args = get_args()
 
     dm = OxfordPetDatamodule(1, data_dir="/home/sami/codes/snn-decolle-segmentation/data/oxford_iiit_pet")
     dm.setup()
 
     train_set = dm.train_set
+    
+    # key-value of 
+    annotation_dict = {}
 
     # get item
     for i in range(len(train_set.images_filenames)):
@@ -49,9 +96,22 @@ if __name__ == "__main__":
         )
 
         mask = preprocess_mask(mask)
-        contour = find_polygon(mask)
 
-        cv2.drawContours(mask, [contour], -1, 1., 3)
-        plt.imshow(mask)
-        plt.show()
-        height, width = mask.shape[0], mask.shape[1]
+        contour = find_polygon(mask, args.polygon_vertices)
+
+        if contour is None:
+            continue
+            
+        contour = np.squeeze(contour).tolist()
+        annotation_dict[image_filename] = contour
+        
+        
+        # cv2.drawContours(mask, [contour], -1, 1., 3)
+        # plt.imshow(mask)
+        # plt.show()
+        # height, width = mask.shape[0], mask.shape[1]
+
+    with open(f'polygons_vertices{args.polygon_vertices}.json', 'w') as file:
+        json.dump(annotation_dict, file)
+        
+    print('done !')
